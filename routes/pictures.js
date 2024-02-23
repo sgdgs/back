dbc14919324e13294c3480a23eb59f98455be6ca
sgdgs <fred.lai.618@gmail.com>
2024-02-23 16:36:39 +0800

    up

--- routes/pictures.js
+++ routes/pictures.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import * as auth from '../middlewares/auth.js'
-import { create, getAll, edit, get, getId, remove } from '../controllers/pictures.js'
+import { create, getAll, remove } from '../controllers/pictures.js'
 import upload from '../middlewares/upload.js'
 import admin from '../middlewares/admin.js'
 
@@ -8,9 +8,7 @@ const router = Router()
 
 router.post('/', auth.jwt, admin, upload, create)
 router.get('/all', getAll)
-router.patch('/:id', auth.jwt, admin, upload, edit)
-router.get('/', get)
-router.get('/:id', getId)
+router.patch('/:id', auth.jwt, admin, upload)
 router.delete('/:id', auth.jwt, admin, remove)
 
 export default router
