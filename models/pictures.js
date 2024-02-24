22f21e489d195eca58ce2733fae162dcd14ee5d4
sgdgs <fred.lai.618@gmail.com>
2024-02-24 10:56:35 +0800

    up

--- models/pictures.js
+++ models/pictures.js
@@ -5,22 +5,6 @@ const schema = new Schema({
     type: String,
     required: [true, '缺少商品圖片']
   }
-  // description: {
-  //   type: String,
-  //   required: [true, '缺少商品說明']
-  // },
-  // category: {
-  //   type: String,
-  //   required: [true, '缺少商品分類'],
-  //   enum: {
-  //     values: ['衣服', '食品', '3C', '遊戲'],
-  //     message: '商品分類錯誤'
-  //   }
-  // },
-  // sell: {
-  //   type: Boolean,
-  //   required: [true, '缺少商品上架狀態']
-  // }
 }, {
   timestamps: true,
   versionKey: false
